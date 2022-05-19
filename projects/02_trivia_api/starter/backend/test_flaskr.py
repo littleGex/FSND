60fa3bc105c65e25eba704ea083e2172294bbb93
littleGex <67484931+littleGex@users.noreply.github.com>
2022-05-19 20:39:07 +0200

    Update to use system enviroment variables for databse connection

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -14,8 +14,12 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.DB_HOST = os.getenv('DB_HOST', 'localhost:5432')
+        self.DB_USER = os.getenv('DB_USERS', 'alanford')
+        self.DB_PASSWORD = os.getenv('DB_PASSWORD', '123456a')  # Dummy password
+        self.DB_NAME = os.getenv('DB_NAME', 'trivia')
+        self.database_path = "postgres://{}:{}@{}/{}".format(
+            self.DB_USER, self.DB_PASSWORD, self.DB_HOST, self.DB_NAME)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
